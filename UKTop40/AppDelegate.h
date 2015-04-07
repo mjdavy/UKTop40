4cb4d740db4661b09281ddf8ae4cbb36e6cd839a
Davy <davym@Martins-MacBook-Pro.local>
2015-04-07 10:00:21 -0400

    Initial Commit

---
+++ UKTop40/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UKTop40
+//
+//  Created by Davy, Martin on 4/7/15.
+//  Copyright (c) 2015 Mandi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UKTop40/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UKTop40
+//
+//  Created by Davy, Martin on 4/7/15.
+//  Copyright (c) 2015 Mandi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
